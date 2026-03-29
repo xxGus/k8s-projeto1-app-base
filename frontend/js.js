a8ef4e44ec0fb073ab1936616fa4df6937978096
Gustavo <dev.gustavo.baptista@gmail.com>
2026-03-29 17:36:55 -0300

    deployment e services adicionados, persistencia de banco de dados e integração com banco de dados e backend

--- frontend/js.js
+++ frontend/js.js
@@ -5,7 +5,7 @@ $("#button-blue").on("click", function() {
     var txt_comentario = $("#comment").val();
 
     $.ajax({
-        url: "",
+        url: "http://192.168.59.100:30915/",
         
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
